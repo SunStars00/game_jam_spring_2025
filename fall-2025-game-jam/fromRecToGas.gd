35d71c586405e82c959f2e6df9bb6fc831e6d120
EngineeringProblems <104740350+EngineeringProblems@users.noreply.github.com>
2025-02-21 15:16:14 -0500

    Delete fall-2025-game-jam directory

--- fall-2025-game-jam/fromRecToGas.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _on_pressed() -> void:
-	%GasRoomBackground.visible = true
-	%RecRoomBackground.visible = false
--- fall-2025-game-jam/fromRecToKit.gd
+++
@@ -1,5 +0,0 @@
-extends Button
-
-func _on_pressed() -> void:
-	%KitBackground.visible = true
-	%RecRoomBackground.visible = false
