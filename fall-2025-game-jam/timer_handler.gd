51dcc4d9607dcff985e65d83e511c7b0eee1cfbc
EngineeringProblems <104740350+EngineeringProblems@users.noreply.github.com>
2025-02-21 14:44:19 -0500

    Version 0.2
    added distraction person code

--- fall-2025-game-jam/timer_handler.gd
+++ fall-2025-game-jam/timer_handler.gd
@@ -1,13 +1,42 @@
 extends Node
 
+var time: float
+var deadTime: float
+var a
+var b
+var rng = RandomNumberGenerator.new()
+var cofMachBroke: bool
+var maxPeople: int
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	%PeopleDistractionTimer.wait_time = 1
+	time = 0
+	deadTime = 60
+	maxPeople = (get_tree().get_nodes_in_group("distractionPeople").size() - 1)
+	get_tree().call_group("distractionPeople", "changeVisByAsk", maxPeople + 1)
+	a = 60 # first wait time
+	b = 260 # decrease rate
+	cofMachBroke = false
+	%PeopleDistractionTimer.wait_time = a
 	%PeopleDistractionTimer.start()
 
+func _process(delta: float) -> void:
+	time + delta
+	if time > deadTime:
+		pass #game end
+
+func difficulty(x) -> float:
+	if cofMachBroke:
+		return a*exp(-x/(b/2))
+	else:
+		return a*exp(-x/b)
+
+func startPeopleTimer() -> void:
+	%PeopleDistractionTimer.wait_time = difficulty(time) + rng.randf_range(0,difficulty(time)/10)
+	%PeopleDistractionTimer.start()
 
 func _on_people_distraction_timer_timeout() -> void:
-	%PeopleDistractionTimer.wait_time = 0.5
-	%PeopleDistractionTimer.start()
-	print("peopleTimer")
+	get_tree().call_group("distractionPeople", "changeVisByAsk", rng.randi_range(0,maxPeople))
+
+func _on_next_break_timer_timeout() -> void:
+	pass # Replace with function body.
